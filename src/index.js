7086e14eaf9862d035d704c39be1a91488a71dce
Thuy Pham <ttpham.2018@sis.smu.edu.sg>
2021-05-07 15:06:18 +0800

    bugs fixed

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import reportWebVitals from './reportWebVitals';
 //   document.getElementById('root')
 // );
                                            
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 
 // If you want to start measuring performance in your app, pass a function
